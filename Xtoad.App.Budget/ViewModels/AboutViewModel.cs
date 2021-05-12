adab0dcdda592b7b972e2272c7aa479cdc854985
442803117 <442803117@qq.com>
2021-05-12 23:49:04 +0800

    嵌入图标字体

--- Xtoad.App.Budget/ViewModels/AboutViewModel.cs
+++ Xtoad.App.Budget/ViewModels/AboutViewModel.cs
@@ -11,8 +11,11 @@ namespace Xtoad.App.Budget.ViewModels
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            RegisterCommand = new Command(async () => await Shell.Current.GoToAsync("//LoginPage"));
         }
 
         public ICommand OpenWebCommand { get; }
+
+        public ICommand RegisterCommand { get; }
     }
 }
