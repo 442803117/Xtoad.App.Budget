6b81076dbd65d6f85572c26ad085fcba25a4561f
442803117 <442803117@qq.com>
2021-05-13 18:48:42 +0800

    修改批复

---
+++ Xtoad.App.Budget/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Xtoad.App.Budget.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        public ProfileViewModel()
+        {
+            Title = "我的";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            RegisterCommand = new Command(async () => await Shell.Current.GoToAsync("//LoginPage"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public ICommand RegisterCommand { get; }
+    }
+}
---
+++ Xtoad.App.Budget/Views/ProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xtoad.App.Budget.Views
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
