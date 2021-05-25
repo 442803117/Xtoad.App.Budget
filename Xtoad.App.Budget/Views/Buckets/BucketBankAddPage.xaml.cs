3d4c8c267d8eba7fb217bbefbc3ef23e36da18e7
442803117 <442803117@qq.com>
2021-05-25 11:11:12 +0800

    新增功能

---
+++ Xtoad.App.Budget/Views/Buckets/BucketBankAddPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xtoad.App.Budget.ViewModels;
+using Xtoad.App.Budget.ViewModels.Buckets;
+
+namespace Xtoad.App.Budget.Views.Buckets
+{
+    public partial class BucketBankAddPage : ContentPage
+    {
+        public BucketBankAddPage()
+        {
+            InitializeComponent();
+            BindingContext = new BucketBankDetailViewModel();
+        }
+    }
+}
